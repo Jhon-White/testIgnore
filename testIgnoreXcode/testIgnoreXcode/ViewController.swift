f5f9c9aded0cb7972ffc35b07f9fa61221b4c89c
John <belousion@yahoo.com>
2022-03-24 10:02:45 +0200

    Start project

---
+++ testIgnoreXcode/testIgnoreXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testIgnoreXcode
+//
+//  Created by John White Mustache on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
